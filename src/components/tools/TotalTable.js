3b5b2096fa644d3a96462eb625222e9413af206e
Daniil-Aleshechkin <aleshechkin.daniil@gmail.com>
2021-04-09 10:13:58 -0600

    total table updating

--- src/components/tools/TotalTable.js
+++ src/components/tools/TotalTable.js
@@ -1,104 +1,92 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import {Card, CardBody} from "shards-react";
+import { Card, CardBody } from "shards-react";
 
-class TotalTable extends Component {
-  constructor(props) {
-    super(props);
-
-    let salesData = Object.values(props.getData().data).slice(-2);
-    let shipmentData = Object.values(props.getData().data).slice(-2);
-    let revenueData = Object.values(props.getData().data).slice(-2);
-    let customerData = Object.values(props.getData().data).slice(-2);
-    this.sale = {
-      count: Math.floor(salesData[0]),
-      percentage: Math.round((salesData[0] - salesData[1]) / salesData[0])
-    };
-    this.ship = {
-      count: Math.floor(shipmentData[0]),
-      percentage: Math.round(
-        (shipmentData[0] - shipmentData[1]) / shipmentData[0]
-      )
-    };
-    this.revenue = {
-      count: Math.floor(revenueData[0]),
-      percentage: Math.round((revenueData[0] - revenueData[1]) / revenueData[0])
-    };
-    this.customer = {
-      count: Math.floor(customerData[0]),
-      percentage: Math.round(
-        (customerData[0] - customerData[1]) / customerData[0]
-      )
-    };
-  }
-
-  formatPercentage = percentage => {
+const TotalTable = ({ getData }) => {
+  const salesData = Object.values(getData().data).slice(-2);
+  const shipmentData = Object.values(getData().data).slice(-2);
+  const revenueData = Object.values(getData().data).slice(-2);
+  const customerData = Object.values(getData().data).slice(-2);
+  const sale = {
+    count: Math.floor(salesData[0]),
+    percentage: Math.round((salesData[0] - salesData[1]) / salesData[0])
+  };
+  const ship = {
+    count: Math.floor(shipmentData[0]),
+    percentage: Math.round(
+      (shipmentData[0] - shipmentData[1]) / shipmentData[0]
+    )
+  };
+  const revenue = {
+    count: Math.floor(revenueData[0]),
+    percentage: Math.round((revenueData[0] - revenueData[1]) / revenueData[0])
+  };
+  const customer = {
+    count: Math.floor(customerData[0]),
+    percentage: Math.round(
+      (customerData[0] - customerData[1]) / customerData[0]
+    )
+  };
+  function formatPercentage(percentage) {
     if (percentage > 0) {
       return `${percentage}% ▲`;
     }
     return `${percentage}% ▼`;
-  };
-  formatColour = percentage => {
+  }
+  function formatColour(percentage) {
     if (percentage > 0) {
       return "success";
     }
     return "danger";
-  };
-
-  render() {
-    return (
-      <Card small className="mb-3">
-        <CardBody>
-            <table className="table table-sm table-hover table-bordered p-1">
-              <thead>
-              <tr>
-                <th scope="col">Parameter</th>
-                <th scope="col">Count</th>
-                <th scope="col" className="text-right">
-                  %
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
-                className={`table-${this.formatColour(this.sale.percentage)}`}>
-                <th scope="row">Sale</th>
-                <td>{this.sale.count}</td>
-                <td className="text-right">
-                  {this.formatPercentage(this.sale.percentage)}
-                </td>
-              </tr>
-              <tr
-                className={`table-${this.formatColour(this.ship.percentage)}`}>
-                <th scope="row">Shipments</th>
-                <td>{this.ship.count}</td>
-                <td className="text-right">
-                  {this.formatPercentage(this.ship.percentage)}
-                </td>
-              </tr>
-              <tr
-                className={`table-${this.formatColour(this.revenue.percentage)}`}>
-                <th scope="row">Revenue</th>
-                <td>{this.revenue.count}</td>
-                <td className="text-right">
-                  {this.formatPercentage(this.revenue.percentage)}
-                </td>
-              </tr>
-              <tr
-                className={`table-${this.formatColour(this.customer.percentage)}`}
-              >
-                <th scope="row">Customers</th>
-                <td>{this.customer.count}</td>
-                <td className="text-right">
-                  {this.formatPercentage(this.customer.percentage)}
-                </td>
-              </tr>
-              </tbody>
-            </table>
-        </CardBody>
-      </Card>
-    );
   }
-}
+
+  return (
+    <Card small className="mb-3">
+      <CardBody>
+        <table className="table table-sm table-hover table-bordered p-1">
+          <thead>
+            <tr>
+              <th scope="col">Parameter</th>
+              <th scope="col">Count</th>
+              <th scope="col" className="text-right">
+                %
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className={`table-${formatColour(sale.percentage)}`}>
+              <th scope="row">Sale</th>
+              <td>{sale.count}</td>
+              <td className="text-right">
+                {formatPercentage(sale.percentage)}
+              </td>
+            </tr>
+            <tr className={`table-${formatColour(ship.percentage)}`}>
+              <th scope="row">Shipments</th>
+              <td>{ship.count}</td>
+              <td className="text-right">
+                {formatPercentage(ship.percentage)}
+              </td>
+            </tr>
+            <tr className={`table-${formatColour(revenue.percentage)}`}>
+              <th scope="row">Revenue</th>
+              <td>{revenue.count}</td>
+              <td className="text-right">
+                {formatPercentage(revenue.percentage)}
+              </td>
+            </tr>
+            <tr className={`table-${formatColour(customer.percentage)}`}>
+              <th scope="row">Customers</th>
+              <td>{customer.count}</td>
+              <td className="text-right">
+                {formatPercentage(customer.percentage)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default TotalTable;
